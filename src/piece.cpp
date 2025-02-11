e6b9b6ef34f2706e66a751e68cc33294a9af9ef2
Remi-Ge <140516143+Remi-Ge@users.noreply.github.com>
2025-02-11 17:02:01 +0100

    Add piece class

---
+++ src/piece.cpp
@@ -0,0 +1,27 @@
+#include "piece.h"
+#include <iostream>
+
+const std::map<PieceType, PieceInfo> Piece::pieceRepresentations = {
+    {PieceType::empty, {' ', ' ', ""}},
+    {PieceType::rook, {'R', 'r', "rook"}},
+    {PieceType::knight, {'N', 'n', "knight"}},
+    {PieceType::bishop, {'B', 'b', "bishop"}},
+    {PieceType::queen, {'Q', 'q', "queen"}},
+    {PieceType::king, {'K', 'k', "king"}},
+    {PieceType::pawn, {'P', 'p', "pawn"}},
+};
+
+Piece::Piece(char notation) {
+    for (const auto& [type, info] : pieceRepresentations) {
+        if (notation == info.whiteNotation) {
+            this->isWhite = true;
+            this->type = type;
+            break;
+        }
+        if (notation == info.blackNotation) {
+            this->isWhite = false;
+            this->type = type;
+            break;
+        }
+    }
+}
---
+++ src/piece.h
@@ -0,0 +1,32 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <map>
+#include <string>
+
+enum class PieceType {
+    empty,
+    rook,
+    knight,
+    bishop,
+    queen,
+    king,
+    pawn
+};
+
+struct PieceInfo {
+    char whiteNotation;
+    char blackNotation;
+    std::string name;
+};
+
+class Piece {
+private:
+    static const std::map<PieceType, PieceInfo> pieceRepresentations;
+public:
+    Piece(char notation);
+    PieceType type = PieceType::empty;
+    bool isWhite = true;
+};
+
+#endif
